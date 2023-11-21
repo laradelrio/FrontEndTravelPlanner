e03607e27cfc46777f4455bf259e962854cd5d77
laradelrio <lara.delrio333@gmail.com>
2023-11-21 17:46:47 +0100

    register as popup

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,21 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, TemplateRef, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Form } from '@app/core/interfaces/form.interface';
 import { FormService } from '@app/shared/services/form.service';
+import { NgbModal, NgbModalConfig, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '@app/shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
+  providers: [NgbModalConfig, NgbModal],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  
-  private formService: FormService;
 
+  //for modal
+  private modalRef!: NgbModalRef;
+  @ViewChild('registerModal') private modalContent!: TemplateRef<RegisterComponent>
+
+  //for form
+  private formService: FormService;
   registerForm: FormGroup;
 
   signUpForm: Form[] = [
@@ -28,6 +35,7 @@ export class RegisterComponent {
   constructor(
     private fb: FormBuilder,
     private router: Router,
+    config: NgbModalConfig, private modalService: NgbModal
   ) {
     this.registerForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(2)]],
@@ -37,6 +45,9 @@ export class RegisterComponent {
     });
 
     this.formService = inject(FormService);
+
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
   
   isValidInput(input: string): boolean | null{
@@ -52,6 +63,20 @@ export class RegisterComponent {
     if(this.registerForm.valid){
       
     }
+  }
+
+ 
+
+  openModal(): Promise<boolean> {
+    return new Promise<boolean>(resolve => {
+      this.modalRef = this.modalService.open(this.modalContent, { size: 'lg', centered: true })
+      // this.modalRef.result.then((result) => {
+      //   console.log(result);
+      //   this.newConfirmationEvent.emit(result);
+      // }, (reason) => {
+      //   console.log(reason);
+      // });
+    })
   }
 
 }
--- src/app/layout/mainpage/header/header.component.ts
+++ src/app/layout/mainpage/header/header.component.ts
@@ -1,26 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '@app/shared/components/modal/modal.component';
+import { RegisterComponent } from '@app/features/auth/register/register.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, ModalComponent],
+  imports: [CommonModule, ModalComponent, RegisterComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  @ViewChild('sharedModal') 
-  private modalComponent!: ModalComponent;
-  modalStyle: string = 'modal-style-primary';
-  modalTitle: string = 'Info Confirmation';
-  modalBody: string = 'This is a Information Confirmation message';
-  modalButtonColor: string = 'btn-primary';
-
-
+  @ViewChild('registerModal') 
+  private registerComponent!: RegisterComponent;
   
   async openModal() {
-    return await this.modalComponent.open();
+    return await this.registerComponent.openModal();
   }
 
   getModalValue(value: any) {
