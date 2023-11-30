efd3b40dec3f9dcbdf50ba51b1fb55f55a92d8f4
laradelrio <lara.delrio333@gmail.com>
2023-11-30 09:32:13 +0100

    create userProfile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainpageComponent } from '@layout/mainpage/mainpage.component';
 import { guardsGuard } from './core/guards/guards.guard';
 import { TestfeatureComponent } from './features/testfeature/testfeature.component';
+import { UserProfileComponent } from './features/user-profile/user-profile.component';
 
 
 export const routes: Routes = [
@@ -19,6 +20,13 @@ export const routes: Routes = [
                 loadComponent : () => 
                     import('@app/features/home/home.component').then((m) => m.HomeComponent)
             },
+            {
+                path: 'profile',
+                loadComponent : () => 
+                    import('@app/features/user-profile/user-profile.component').then((m) => m.UserProfileComponent),
+                canActivate: [guardsGuard],
+            },
+
             {
                 path: '**',
                 loadComponent : () => 
