059dac36bc5bcd524b3b7d6c3309c9341226f34e
laradelrio <lara.delrio333@gmail.com>
2024-01-10 13:17:17 +0100

    delete console.log

--- src/app/shared/services/form.service.ts
+++ src/app/shared/services/form.service.ts
@@ -45,7 +45,6 @@ export class FormService {
           errorMessage = `Must have at least ${errors['minlength']['requiredLength']} characters`;
           break;
         case 'maxlength':
-          console.log('error', errors)
           errorMessage = `Maximum ${errors['maxlength']['requiredLength']} characters`;
           break;
       }
