310db2d04dbf8a3f03e2c7ee9ac92da547642c0f
laradelrio <lara.delrio333@gmail.com>
2024-01-16 10:21:10 +0100

    open register and login modal from home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { RegisterComponent } from '../auth/register/register.component';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '@app/shared/components/modal/modal.component';
+import { LoginComponent } from '../auth/login/login.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ModalComponent, RegisterComponent, LoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  @ViewChild('registerModal') 
+  private registerComponent!: RegisterComponent;
+  private modalRef!: NgbModalRef;
+
+  @ViewChild('loginModal') 
+  private loginComponent!: LoginComponent;
+
+  openRegister() {
+    this.openModalRegister();
+  }
+
+  async openModalRegister() {
+    return await this.registerComponent.openModal();
+  }
+
+  openLogin() {
+    this.openModalLogin();
+  }
+  
+  async openModalLogin() {
+    return await this.loginComponent.openModal();
+  }
 
 }
