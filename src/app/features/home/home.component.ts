12b7d2233fedad6686fc0d0155b7e7033ce1f358
laradelrio <lara.delrio333@gmail.com>
2024-02-02 11:01:56 +0100

    home page redesign

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,28 +6,89 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from '@app/shared/components/modal/modal.component';
 import { LoginComponent } from '../auth/login/login.component';
 import { UserApiService } from '@app/core/apiServices/user-api.service';
+import { UserCardComponent } from '@app/shared/components/user-card/user-card.component';
+import { UserData } from '@app/core/interfaces/user.interface';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, ModalComponent, RegisterComponent, LoginComponent],
+  imports: [CommonModule, RouterLink, ModalComponent, RegisterComponent, LoginComponent, UserCardComponent, CarouselModule, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  @ViewChild('registerModal') 
+  @ViewChild('registerModal')
   private registerComponent!: RegisterComponent;
   private modalRef!: NgbModalRef;
 
-  @ViewChild('loginModal') 
+  @ViewChild('loginModal')
   private loginComponent!: LoginComponent;
 
+
   private userService: UserApiService = inject(UserApiService);
-  
-  constructor(){
-    effect(()=>{
-        if(this.userService.openLogin()){
+  public userProfiles: UserData[] = [
+    {
+      id: 1,
+      name: 'Jenifer',
+      email: 'jenifer@mail.com',
+      photo: 'http://tinyurl.com/4af7t7cb',
+      matchPercentage: 33,
+    },
+    {
+      id: 1,
+      name: 'Josue',
+      email: 'josue@mail.com',
+      photo: 'https://i.pinimg.com/736x/a8/06/0a/a8060af01fe6971b37f945fb34275acb.jpg',
+      matchPercentage: 66,
+    },
+    {
+      id: 1,
+      name: 'Marco',
+      email: 'manolo@mail.com',
+      photo: 'http://tinyurl.com/3ur52s76',
+      matchPercentage: 33,
+    },
+    {
+      id: 1,
+      name: 'Fatima',
+      email: 'fatima@mail.com',
+      photo: 'http://tinyurl.com/4885aue5',
+      matchPercentage: 66,
+    },
+    {
+      id: 1,
+      name: 'Lucy',
+      email: 'lucy@mail.com',
+      photo: 'http://tinyurl.com/26nntf4t',
+      matchPercentage: 66,
+    }
+  ];
+
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1199px',
+      numVisible: 1,
+      numScroll: 1
+    },
+    {
+      breakpoint: '991px',
+      numVisible: 2,
+      numScroll: 1
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ];
+
+  constructor() {
+    effect(() => {
+      if (this.userService.openLogin()) {
         this.openLogin();
       }
     })
@@ -44,7 +105,7 @@ export class HomeComponent {
   openLogin() {
     this.openModalLogin();
   }
-  
+
   async openModalLogin() {
     return await this.loginComponent.openModal();
   }
