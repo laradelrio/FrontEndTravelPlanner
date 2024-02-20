6b7a11ebf7f63829ebb2a8971391b11f4963664c
laradelrio <lara.delrio333@gmail.com>
2024-02-20 20:36:30 +0100

    api key change

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    imgbbApiKey: 'f2ffe92cd6793ecf39f3af3e4852d30a',
-    unsplashClientID: '_wwGZJTpge9ugVwA6kfx0JNteyRmRwU-CwTLrVO_SWk',
-    mapBoxApiKey: 'pk.eyJ1IjoibDRyNCIsImEiOiJjbHNtZHI0ZTkwZWtrMmxvZXFrMmJxYzE5In0.CeA8BpnFwlRkgfB1QJEGQw',
+    imgbbApiKey: '',
+    unsplashClientID: '',
+    mapBoxApiKey: '',
 };
